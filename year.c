40e8f21f6e9a7231ff9420f99fd81f01f276b75b
projach <iakovosplane123@gmail.com>
2020-11-22 18:19:40 +0200

    edited

--- year.c
+++ year.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//this is a method to see if the year is leap or no
 void isLeap(int year){
 	
 	if (year % 4 == 0 && (year % 100 !=0 || year % 400 == 0)){
@@ -8,18 +9,19 @@ void isLeap(int year){
 			printf("%d is not a leap year.", year);
 		}
 	}
-		
-void whenIsEaster(year){
+//this is a method to find what date is the easter in every year
+void whenIsEaster(int year){
 	
 	printf("\n");
 	   int a,b,c,d,e,dayOfEaster;
-	   
+	   //this is the formula to find easter
 	   a = year % 19;
 	   b = year % 4;
 	   c = year % 7;
 	   d = (19*a + 16) % 30;
 	   e = (2*b + 4*c+6*d) % 7;
 	   dayOfEaster = 3 + d + e;
+	   //this is to see if the easter is in april or may
 	   if(dayOfEaster <= 30){
 	   	printf("Easter is in %d april %d \n", dayOfEaster,year);
 	   	printf("\n");
@@ -32,9 +34,10 @@ void whenIsEaster(year){
 	   	printf("\n");
 	   }
 }
-
+//this is the method who constructs the program
 void programStart(){
 	int year;
+	// this is a for loop to go through 2010-2030
 for(year = 2010; year <= 2030; year++){
 		isLeap(year);
 		whenIsEaster(year);
